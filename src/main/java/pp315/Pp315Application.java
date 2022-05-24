f17f34360fb427a717328f85278100185e541f19
Soundgraf <soundgraf@mail.ru>
2022-05-25 01:17:21 +0300

    Initial commit

---
+++ src/main/java/pp315/Pp315Application.java
@@ -0,0 +1,13 @@
+package pp315;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp315Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pp315Application.class, args);
+    }
+
+}
---
+++ src/main/java/pp315/configuration/AppConfig.java
@@ -0,0 +1,16 @@
+package pp315.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@ComponentScan("pp315")
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
